b38ba601d59bf7490acaaf577fdf74c68c33424e
Linda Hrdinová <hrlin@seznam.cz>
2024-08-27 13:15:01 +0200

    NxM works

--- src/main/java/com/github/LindaHrdinova/RadialReach.java
+++ src/main/java/com/github/LindaHrdinova/RadialReach.java
@@ -5,41 +5,58 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RadialReach {
+    private static void inputCheck(int input) {
+        if (input % 2 == 0 || input <= 1) {
+            System.err.println("This is not an odd number bigger than 1.");
+            throw new IllegalArgumentException("This is not an odd number bigger than 1.");
+        };
+    }
+
     public static void main(String[] args) {
 
         // Loading board size
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter an odd number bigger then 1:");
+        System.out.println("Enter an odd number bigger then 1, side A:");
         int boardSizeA = Integer.parseInt(scanner.nextLine());
 
-        // filling
-        int filling = boardSizeA - 1;
+        inputCheck(boardSizeA);
 
         if (boardSizeA % 2 == 0 || boardSizeA <= 1) {
             System.err.println("This is not an odd number bigger than 1.");
             return;
-        } else {
-            int[][] board = new int[boardSizeA][boardSizeA];
-            System.out.println("The size is: " + boardSizeA);
-            for (int i = 0; i < boardSizeA; i++) {
-                for (int j = 0; j < boardSizeA; j++) {
-                    board[i][j] = filling;
-                    System.out.print(board[i][j] + "\t");
+        }
 
+        System.out.println("Enter an odd number bigger then 1, side B:");
+        int boardSizeB = Integer.parseInt(scanner.nextLine());
 
-                    if (j < board.length / 2) {
-                        filling--;
-                    } else {
-                        filling++;
-                    }
-                }
-
-                if (i < board.length / 2) {
-                    filling = filling - 2;
-                };
-                System.out.println();
-            }
+        if (boardSizeB % 2 == 0 || boardSizeB <= 1) {
+            System.err.println("This is not an odd number bigger than 1.");
             return;
         }
+
+        int filling = ((boardSizeA - 1) / 2 + (boardSizeB - 1) / 2);
+
+        int[][] board = new int[boardSizeA][boardSizeB];
+        System.out.println("The size is: " + boardSizeA + " x " + boardSizeB);
+        for (int i = 0; i < boardSizeA; i++) {
+            for (int j = 0; j < boardSizeB; j++) {
+                board[i][j] = filling;
+                System.out.print(board[i][j] + "\t");
+
+
+                if (j < boardSizeB / 2) {
+                    filling--;
+                } else {
+                    filling++;
+                }
+            }
+
+            if (i < boardSizeA / 2) {
+                filling = filling - 2;
+            }
+            ;
+            System.out.println();
+        }
+        return;
     }
 }
